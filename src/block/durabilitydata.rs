c8c61affa2b5c5e09184b79922e4dab3805c392c
Zatzou <zatzou@zatzou.com>
2025-01-06 21:25:55 +0200

    rip

--- src/block/durabilitydata.rs
+++ src/block/durabilitydata.rs
@@ -13,6 +13,7 @@ use super::{AnyBlock, DataBlockId};
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct DurabilityData {
     /// The effect strength of the item is the overall effectiveness of the identifications on the item. (the percentage shown next to the item name)
+    // TODO: fix misspelling in the next minor version
     pub effect_strenght: u8,
     /// Current durability of the item
     pub current: i32,
